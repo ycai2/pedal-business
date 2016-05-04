3d3ee6d6b96c4b152794136dbb55e3279944e918
wyh4788 <yinghuanwang521@gmail.com>
2016-05-04 11:48:47 -0400

    err 422 could not register

--- static/signup.js
+++ static/signup.js
@@ -12,7 +12,6 @@
   $(function() {
 
     // The DOM is ready!
-    console.log("haha");
     var table = $('.ui-timepicker-input');
     for (var i = 0; i < table.length; i++){
       $(table[i]).timepicker();
@@ -21,76 +20,128 @@
 
   // The rest of the code goes here!
   // 'blt5d4sample2633b' is a dummy Application API key
-  var app = Built.App('blt6f9b218391b44387');
-  var user = app.User();
+  //var app = Built.App('blt6f9b218391b44387');
+  //var user = app.User();
 
-  function get_data(business_id) {
-    var business = app.Class('business').Object(business_id);
-    
-    business.fetch().then(function(business){
-      var b = business.toJSON();
-      $('#res_name').val(b.res_name);
-      $('#street').val(b.address.street);
-      $('#city').val(b.address.city);
-      $('#state').val(b.address.state);
-      $('#phone').val(b.phone);
-      $('#description').val(b.description);
-      $('#deals').val(b.deals);
+  var $username;
+  var $password;
+  var $password_confirm;
+  var username_email;
 
-      var table = $('.table td');
-      var day = "sun";
-      var hours = {};
+  // if (password != password_confirm){
+  //   console.log
+  // }
 
-      for (var i = 0; i < table.length; i++){
-        if (i%3 == 0){
-          day = $(table[i]).attr('id');
-        }else if (i%3 == 1){
-          $(table[i]).find('input').val(b['hours'][day]['open']);
-        }else{
-          $(table[i]).find('input').val(b['hours'][day]['close']);          
-        }
-      } 
+  $('#submit-signup').click(function(){
+    var account_info = $('.account-info input');
+    account_info.each(function(index){
+      //check if every field is filled
+      if ($(this).val() == "" || $(this).val() == undefined){
+        console.log("hahahaha");
+        $('#notifier').text("please fill in required fields!");
+        return;
+      }
+    }); 
 
-      $('#submit-details').click(function(){
-        for (var i = 0; i < table.length; i++){
-          if (i%3 == 0){
-            day = $(table[i]).attr('id');
-            hours[day] = {'open': '', 'close': ''};
-          }else if (i%3 == 1){
-            hours[day]['open'] = $(table[i]).find('input').val();
-          }else{
-            hours[day]['close'] = $(table[i]).find('input').val();
-          }
-        }
-        // console.log(hours);
-        // console.log(b['hours']);
+    username = $('#username');
+    password = $('#password');
+    password_confirm = $('#password-confirm');
 
-        business = business
-                   .set('res_name', $('#res_name').val())
-                   .set('address', {
-                                    'street': $('#street').val(),
-                                    'city': $('#city').val(),
-                                    'state': $('#state').val()
-                                  })
-                   .set('phone', $('#phone').val())
-                   .set('description', $('#description').val())
-                   .set('hours', hours)
-                   .set('deals', $('#deals').val());
+    //check if the passwords are the same
+    if (password.val() != password_confirm.val()){
+      $('#notifier').text("please enter the same password!");
+      return;
+    }
 
-        business
-        .save()
-        .then(function(business){
-          console.log(business.toJSON());
-        }, function(error){
-          console.log(error);
-        });
-      });
-      
+    username_email = ('#username');
+    var username_email = username.val() + "@pedal.com";
+    console.log(username_email);
+    console.log(password.val());
+    console.log(password_confirm.val());
+
+    var user = Built.App('blt6f9b218391b44387').User();
+    user
+    .register("1020967309@qq.com", "wyh4788", "wyh4788")
+    .then(function(user) {
+        // user registered successfully
+        console.log(user.toJSON())
     }, function(error) {
-    // some error has occurred
-    // refer to the 'error' object for more details
+        console.log(error);
+        // some error has occurred
+        // refer to the 'error' object for more details
     });
-  };
+
+  });
+
+
+  // function get_data(business_id) {
+  //   var business = app.Class('business').Object(business_id);
+    
+  //   // Retrive data from backend
+  //   business.fetch().then(function(business){
+  //     var b = business.toJSON();
+  //     $('#res_name').val(b.res_name);
+  //     $('#street').val(b.address.street);
+  //     $('#city').val(b.address.city);
+  //     $('#state').val(b.address.state);
+  //     $('#phone').val(b.phone);
+  //     $('#description').val(b.description);
+  //     $('#deals').val(b.deals);
+
+  //     var table = $('.table td');
+  //     var day = "sun";
+  //     var hours = {};
+
+  //     for (var i = 0; i < table.length; i++){
+  //       if (i%3 == 0){
+  //         day = $(table[i]).attr('id');
+  //       }else if (i%3 == 1){
+  //         $(table[i]).find('input').val(b['hours'][day]['open']);
+  //       }else{
+  //         $(table[i]).find('input').val(b['hours'][day]['close']);          
+  //       }
+  //     } 
+
+  //     $('#submit-details').click(function(){
+  //       for (var i = 0; i < table.length; i++){
+  //         if (i%3 == 0){
+  //           day = $(table[i]).attr('id');
+  //           hours[day] = {'open': '', 'close': ''};
+  //         }else if (i%3 == 1){
+  //           hours[day]['open'] = $(table[i]).find('input').val();
+  //         }else{
+  //           hours[day]['close'] = $(table[i]).find('input').val();
+  //         }
+  //       }
+  //       // console.log(hours);
+  //       // console.log(b['hours']);
+
+  //       business = business
+  //                  .set('res_name', $('#res_name').val())
+  //                  .set('address', {
+  //                                   'street': $('#street').val(),
+  //                                   'city': $('#city').val(),
+  //                                   'state': $('#state').val()
+  //                                 })
+  //                  .set('phone', $('#phone').val())
+  //                  .set('description', $('#description').val())
+  //                  .set('hours', hours)
+  //                  .set('deals', $('#deals').val());
+
+  //       business
+  //       .save()
+  //       .then(function(business){
+  //         console.log(business.toJSON());
+  //       }, function(error){
+  //         console.log(error);
+  //       });
+  //     });
+      
+  //   }, function(error) {
+  //   // some error has occurred
+  //   // refer to the 'error' object for more details
+  //   });
+  // };
 
 
   
