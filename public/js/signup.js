874a58820071490e9e85dfd08542251e639ff3ee
Yisheng Cai <cai.eason@gmail.com>
2016-05-20 04:04:01 -0400

    finished sign-up

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,29 +1,61 @@
 $(function(){
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      console.log('a user is signed in');
+      $('#signup-form').closeModal();
+      Materialize.toast('Account created!', 3000);
 
-  $('.modal-action').click(function(){
-    var name = $('#new_business_name').val();
-    var pw = $('#new_password').val();
-    var pw_confirm = $('#new_password_confirm').val();
+      var login = document.getElementById('login-card');
+      login.setAttribute('hidden', 'true');
+    } else {
+      // No user is signed in.
+      $('.modal-action').click(function(){
 
-    if (!name) {
-      Materialize.toast('Username is empty!', 3000, 'short-toast');
-    } else if (/(\w+(-\w+)*)/.test(name)) {
-      Materialize.toast('Username must be lowercase letters connected by hyphens', 3000, 'short-toast');
+        var name = $('#new_business_name').val();
+        var pw = $('#new_password').val();
+        var pw_confirm = $('#new_password_confirm').val();
 
+        var verified_u = null;
+        var verified_p = null;
+
+        if (!name) {
+          Materialize.toast('Username is empty!', 3000, 'short-toast');
+        } else if (!/(^\w+(-\w+)*$)/.test(name)) {
+          Materialize.toast('Username must be lowercase letters connected by hyphens', 3000, 'short-toast');
+        } else {
+          verified_u = name;
+        }
+
+        if (!pw && !pw_confirm) {
+          Materialize.toast('Password is empty', 3000, 'short-toast');
+        }
+        else if (pw != pw_confirm) {
+          Materialize.toast('Passwords do not match!', 3000, 'short-toast');
+        }
+        else {
+          verified_p = pw;
+        }
+
+        if (verified_u && verified_p) {
+          console.log('signup');
+          firebase.auth().createUserWithEmailAndPassword(verified_u + "@pedal.com", verified_p).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            Materialize.toast(errorMessage, 3000, 'short-toast');
+            // ...
+          });
+          
+        }
+
+        
+
+      });
     }
-
-    if (!pw && !pw_confirm) {
-      Materialize.toast('Password is empty', 3000, 'short-toast');
-    }
-    else if (!(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}/).test(pw)) {
-      Materialize.toast('Password must have at least six characters that are letters, numbers or the underscore and at least one number, one lowercase and one uppercase letter.', 3000, 'short-toast');
-    } 
-    else if (pw != pw_confirm) {
-      Materialize.toast('Passwords do not match!', 3000, 'short-toast');
-    }
-
-
-
-
   });
+
+
+  
 });
