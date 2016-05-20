96d8d29c7e352779a6f8bc8d521ac261af452757
Yisheng Cai <cai.eason@gmail.com>
2016-05-20 16:00:09 -0400

    implemented login and logout

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,15 +1,45 @@
+var login = document.getElementById('login-card');
+var interface = document.getElementById('interface-card');
 $(function(){
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
       console.log('a user is signed in');
       $('#signup-form').closeModal();
-      Materialize.toast('Account created!', 3000);
-
-      var login = document.getElementById('login-card');
+      
       login.setAttribute('hidden', 'true');
+      interface.removeAttribute('hidden');
+
+      $('#logout-btn').click(function() {
+        firebase.auth().signOut().then(function() {
+          // Sign-out successful.
+          login.removeAttribute('hidden');
+          interface.setAttribute('hidden', 'true');
+        }, function(error) {
+          // An error happened.
+          Materialize.toast(error.message, 3000);
+        });
+      });
+
+      
+
     } else {
       // No user is signed in.
+      interface.setAttribute('hidden', 'true');
+      $('#login-btn').click(function() {
+        var username = $('#username').val();
+        var pw = $('#password').val();
+        $('#password').val("");          //clear password for security reason at front-end
+
+        firebase.auth().signInWithEmailAndPassword(username + "@pedal.com", pw).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          Materialize.toast(errorMessage, 3000);
+          // ...
+        });
+      });
+
       $('.modal-action').click(function(){
 
         var name = $('#new_business_name').val();
@@ -19,6 +49,7 @@ $(function(){
         var verified_u = null;
         var verified_p = null;
 
+        //Validate username.
         if (!name) {
           Materialize.toast('Username is empty!', 3000, 'short-toast');
         } else if (!/(^\w+(-\w+)*$)/.test(name)) {
@@ -27,6 +58,7 @@ $(function(){
           verified_u = name;
         }
 
+        //Validate password
         if (!pw && !pw_confirm) {
           Materialize.toast('Password is empty', 3000, 'short-toast');
         }
@@ -37,6 +69,7 @@ $(function(){
           verified_p = pw;
         }
 
+        //If both valid, create the account
         if (verified_u && verified_p) {
           console.log('signup');
           firebase.auth().createUserWithEmailAndPassword(verified_u + "@pedal.com", verified_p).catch(function(error) {
@@ -47,8 +80,8 @@ $(function(){
             Materialize.toast(errorMessage, 3000, 'short-toast');
             // ...
           });
-          
         }
+
 
         
 
