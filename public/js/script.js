307eadb1269e810e11197be5d6a3daa0873bd8f7
Yisheng Cai <cai.eason@gmail.com>
2016-06-24 15:05:27 -0400

    working portal with ui-router

---
+++ public/js/script.js
@@ -0,0 +1,56 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('pedal', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider) {
+      // For any unmatched url, redirect to /state1
+      $urlRouterProvider.when("", "/404");
+      $urlRouterProvider.otherwise("/404");
+
+
+      $stateProvider
+        .state('home', {
+          url: "/",
+          templateUrl: "pages/home.html"
+        })
+        .state('register', {
+          url: "/register",
+          templateUrl: "pages/register.html"
+        })
+        .state('signin', {
+          url: "/signin",
+          templateUrl: "pages/signin.html"
+        })
+        .state('profile', {
+          url: "/profile",
+          templateUrl: "pages/profile.html"
+        })
+        .state('about', {
+          url: "/about",
+          templateUrl: "pages/about.html"
+        })
+        .state('404', {
+          url: "/404",
+          templateUrl: "pages/404.html"
+        })
+    })
+    .controller('MainController', MainController);
+
+
+
+
+  function MainController() {
+    var vm = this;
+    vm.message = 'Everyone come and see how good I look!';
+  }
+
+  // function AboutController() {
+  //   var vm = this;
+  //   vm.message = 'About page this is';
+  // }
+  // function ContactController() {
+  //   var vm = this;
+  //   vm.message = 'Contact page this is';
+  // }
+})();
